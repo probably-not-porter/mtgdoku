b75ec2fb5cc692a3c8b446b500b3c354183793d0
Porter Libby <pelibby16@earlham.edu>
2025-01-18 14:34:57 -0500

    fix bug for daily puzzle loader

--- js/loaders.js
+++ js/loaders.js
@@ -2,7 +2,7 @@ function load_random(){
     window.location = window.location.href.split("?")[0] + "?" + Date.now();
 }
 function load_daily(){
-    let day = parseInt(new Date().toJSON().slice(0, 10).replace("-",""));
+    let day = parseInt(new Date().toJSON().slice(0, 10).replaceAll("-",""));
     window.location = window.location.href.split("?")[0] + "?" + day;
 }
 function load_from_paste(){
