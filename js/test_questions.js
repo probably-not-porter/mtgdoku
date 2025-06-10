110ca92801279965e0d06f3ce735e8a1db27df9f
Porter Libby <pelibby16@earlham.edu>
2025-06-09 23:59:48 -0400

    improve testing efficiency

--- js/test_questions.js
+++ js/test_questions.js
@@ -1,50 +1,62 @@
-const elems = ["a1","a2","a3","a4","a5","a6","a7","a8","a9"]
+const elems = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9"];
 
-async function test_questions(q){
+async function test_questions(q) {
+    if (!q) return false;
+
     let loaded = 0;
-    if (q == null){ return false } // make sure that there are row/col selections (not null)
-    for (x = 0; x < 3; x++){            // item in header ROW
-        let q1 = q[0][x];               // header row item X
-        for (y = 0; y < 3; y++){        // item in header COL
-            let q2 = q[1][y];           // header col item Y
-            let query = "";                                                     // start query for scryfall
-            if (q1.length == 1){ query += `c%3A${q1.toLowerCase()}` }           // header row if COLOR
-            else if (q1.length < 4){ query += `mv%3D${q1.replace("c","")}` }    // header row if CMC
-            else { query += `t%3A${q1.toLowerCase()}` }                         // header row if TYPE
-            query += "+";                                                       // add separator
-            if (q2.length == 1){ query += `c%3A${q2.toLowerCase()}` }           // header col if COLOR
-            else if (q2.length < 4){ query += `mv%3D${q2.replace("c","")}` }    // header col if CMC
-            else { query += `t%3A${q2.toLowerCase()}` }                         // header col if TYPE
-            
-            const response = await fetch(`https://api.scryfall.com/cards/search?order=cmc&q=${query}`);
-            const data = await response.json();
-            //let best = calc_best(data); // TDOD: should refactor this
-            if (data.total_cards == undefined){ return false} // check if any row/col pairs have NO possible answers
-            else{
-                loaded += 1;
-                document.getElementById('loader').innerHTML = `Checking puzzle (${(loaded / 9 * 100).toFixed(0)}%)`;
-                document.getElementById(elems[x + (y*3)]).innerHTML = `<br>
-                    ${data.total_cards}<br><br>
-                    <button onclick='select("${q1}","${q2}",${x},${y})'>Answer</button>
-                `;
-            }
+    const total = 9;
+
+    const fetches = [];
+
+    for (let x = 0; x < 3; x++) {
+        const q1 = q[0][x];
+        for (let y = 0; y < 3; y++) {
+            const q2 = q[1][y];
+            let query = "";
+
+            if (q1.length === 1) query += `c%3A${q1.toLowerCase()}`;
+            else if (q1.length < 4) query += `mv%3D${q1.replace("c", "")}`;
+            else query += `t%3A${q1.toLowerCase()}`;
+
+            query += "+";
+
+            if (q2.length === 1) query += `c%3A${q2.toLowerCase()}`;
+            else if (q2.length < 4) query += `mv%3D${q2.replace("c", "")}`;
+            else query += `t%3A${q2.toLowerCase()}`;
+
+            const url = `https://api.scryfall.com/cards/search?order=cmc&q=${query}`;
+
+            // Create fetch Promise that updates progress
+            const fetchPromise = fetch(url)
+                .then((res) => res.json())
+                .then((data) => {
+                    if (!data || data.total_cards === undefined)
+                        throw new Error("Invalid data");
+
+                    // Update progress here
+                    loaded++;
+                    document.getElementById("loader").innerHTML =
+                        `Checking puzzle (${((loaded / total) * 100).toFixed(0)}%)`;
+
+                    return { x, y, q1, q2, data };
+                })
+                .catch((err) => null);
+
+            fetches.push(fetchPromise);
         }
     }
-    return true // if nothing has returned false by now, the board is valid.
-}
 
-// function calc_best(data){
-//     let best_price = 0;
-//     let name = "";
-//     if (data.data != null){
-//         for (const [key, value] of Object.entries(data.data)) {
-//             if (value.prices.usd > best_price){
-//                 best_price = value.prices.usd;
-//                 name = value.name;
-//             }
-            
-//         }
-//     }
-//     console.log(name, best_price);
-    
-// }
+    const results = await Promise.all(fetches);
+
+    for (const result of results) {
+        if (!result) return false; // One or more requests failed or returned invalid data
+
+        const { x, y, q1, q2, data } = result;
+        document.getElementById(elems[x + y * 3]).innerHTML = `<br>
+            ${data.total_cards}<br><br>
+            <button onclick='select("${q1}","${q2}",${x},${y})'>Answer</button>
+        `;
+    }
+
+    return true;
+}
